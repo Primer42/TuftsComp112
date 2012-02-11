571b8bc5f3b7039e1f53c0d6e9bbdee4534ead74
William Richard <wrichard@tufts.edu>
2012-02-11 15:46:15 -0500

    Client now does what it needs to. Is not very communication efficient, especially if there are many missing packets, but it works

--- a2/client.c
+++ a2/client.c
@@ -10,6 +10,8 @@
 
 #include "help.h" 
 
+#include <sys/stat.h>
+
 #define TRUE  1
 #define FALSE 0
 
@@ -86,86 +88,100 @@ int main(int argc, char **argv)
     int bitArrayAllocd = FALSE;
 
     //open the file we're asking for
-    int outFd = open(filename,'O_RDRW');
+    char clientFilePath[FILENAMESIZE];
+    strncat(clientFilePath, "./", 2);
+    strncat(clientFilePath, filename, strlen(filename));
+    int outFd;
+    if((outFd = open(clientFilePath, O_RDWR | O_CREAT, 0666)) < 0) {
+      perror("Unable to create client file");
+      exit(2);
+    }
 
+    int done = FALSE;
 /* receive the whole document and make naive assumptions */ 
     //finish when the bit array has been set and it is empty
     //i.e all blocks of the file have been marked as recieved
-    while (!bitArrayAllocd && bits_empty(&blocksNeeded)) { 
-        int retval; 
-again: 
-	if ((retval = select_block(sockfd, 1, 0))==0) { 
+    while (!done) {
+      int retval; 
+    again: 
+      if(done) {
+	break;
+      }
+      
+      if ((retval = select_block(sockfd, 1, 0))==0) { 
         /* timeout */ 
-	  //check if we've started getting blocks already
-	  if(!bitArrayAllocd) {
-	    //we've never gotten a responce
-	    //bad news
-	    perror("Timeout without recieving any blocks");
-	    exit(1);
-	  } else {
-	    //send a request for the blocks we need
-	    //do this badly for now - would need to rewrite
-	    //send_command to do better
-	    //baby steps
-	    int block;
-	    for(block = 0; block < blocksNeeded.nbits; ++block) {
-	      if(bits_testbit(&blocksNeeded, block)) {
-		//still need this block - request it
-		send_command(sockfd, server_dotted, server_port, filename, block, block);
-	      }
+	fprintf(stderr, "Timeout\n");
+	//check if we've started getting blocks already
+	if(!bitArrayAllocd) {
+	  //we've never gotten a responce
+	  //bad news
+	  fprintf(stderr, "Timeout without recieving any blocks");
+	  exit(1);
+	} else {
+	  //send a request for the blocks we need
+	  //do this badly for now - would need to rewrite
+	  //send_command to do better
+	  //baby steps
+	  int block;
+	  for(block = 0; block < blocksNeeded.nbits; ++block) {
+	    if(bits_testbit(&blocksNeeded, block)) {
+	      //still need this block - request it
+	      send_command(sockfd, server_dotted, server_port, filename, block, block);
 	    }
-	    goto again;
 	  }
-	  
-       } else if (retval<0) { 
+	}
+	
+      } else if (retval<0) { 
 	/* error */ 
-	    perror("select"); 
-	    fprintf(stderr, "client: receive error\n"); 
-       } else { 
-	 /* input is waiting, read it */ 
-	    struct sockaddr_in resp_sockaddr; 	/* address/port pair */ 
-	    int resp_len; 			/* length used */ 
-	    char resp_dotted[INET_ADDRSTRLEN]; 	/* human-readable address */ 
-	    int resp_port; 			/* port */ 
-	    int resp_mesglen; 			/* length of message */ 
-	    struct block one_block; 
-
+	perror("select"); 
+	fprintf(stderr, "client: receive error\n"); 
+      } else { 
+	/* input is waiting, read it */ 
+	struct sockaddr_in resp_sockaddr; 	/* address/port pair */ 
+	int resp_len; 			/* length used */ 
+	char resp_dotted[INET_ADDRSTRLEN]; 	/* human-readable address */ 
+	int resp_port; 			/* port */ 
+	int resp_mesglen; 			/* length of message */ 
+	struct block one_block; 
+	
 	/* use helper routine to receive a block */ 
-	    recv_block(sockfd, &one_block, &resp_sockaddr);
-
+	recv_block(sockfd, &one_block, &resp_sockaddr);
+	
 	/* get human-readable internet address */
-	    inet_ntop(AF_INET, (void *)&(resp_sockaddr.sin_addr.s_addr),  
-		resp_dotted, INET_ADDRSTRLEN);
-  	    resp_port = ntohs(resp_sockaddr.sin_port); 
-
-	    fprintf(stderr, "client: %s:%d sent %s block %d (range 0-%d)\n",
+	inet_ntop(AF_INET, (void *)&(resp_sockaddr.sin_addr.s_addr),  
+		  resp_dotted, INET_ADDRSTRLEN);
+	resp_port = ntohs(resp_sockaddr.sin_port); 
+	
+	fprintf(stderr, "client: %s:%d sent %s block %d (range 0-%d)\n",
 		resp_dotted, resp_port, one_block.filename, 
 		one_block.which_block, one_block.total_blocks);
-
+	
 	/* check block data for errors */
-	    if (strcmp(filename, one_block.filename)!=0) { 
-		fprintf(stderr, 
-		    "client: received block with incorrect filename %s\n", 
-		    one_block.filename); 
-		goto again; 
-	    } 
-	    
-	    //first, make the bit array if it has not already been made
-	    if(!bitArrayAllocd) {
-	      bits_alloc(&blocksNeeded, one_block.total_blocks);
-	      bit_setrange(&blocksNeeded, 0, one_block.total_blocks-1);
-	      bitArrayAllocd = TRUE;
-	    }
-
-	    //next, write out the block
-	    lseek(outfd, one_block.which_block*PAYLOADSIZE, SEEK_SET);
-	    write(outfd, one_block.payload, one_block.paysize);
-
-	    //finally, mark the block as recieved
-	    bits_clearbit(&blocksNeeded, one_block.which_block)
+	if (strcmp(filename, one_block.filename)!=0) { 
+	  fprintf(stderr, 
+		  "client: received block with incorrect filename %s\n", 
+		  one_block.filename); 
+	  goto again; 
 	}
-
-	close(outFd)
-    } 
+	
+	//first, make the bit array if it has not already been made
+	if(!bitArrayAllocd) {
+	  bits_alloc(&blocksNeeded, one_block.total_blocks);
+	  bits_clearall(&blocksNeeded);
+	  bits_setrange(&blocksNeeded, 0, one_block.total_blocks-1);
+	  bitArrayAllocd = TRUE;
+	}
+	
+	//next, write out the block
+	lseek(outFd, one_block.which_block*PAYLOADSIZE, SEEK_SET);
+	write(outFd, one_block.payload, one_block.paysize);
+	
+	//finally, mark the block as recieved
+	bits_clearbit(&blocksNeeded, one_block.which_block);	
+      }
+      //see if we're done
+      done = bits_empty(&blocksNeeded);
+    }
+    close(outFd);
 }
 
