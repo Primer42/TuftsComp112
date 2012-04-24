92ae5e9e6d8719b2b4391bc713182a988423398f
William Richard <wrichard@tufts.edu>
2012-04-24 18:40:58 -0400

    Removed host array from nose.h - decided it shouldn't be there

--- a6/nose.h
+++ a6/nose.h
@@ -9,18 +9,6 @@ typedef struct _host_record{
   time_t lastSeenAt;
 } hostRecord;
 
-/*This is the array of host records.
-It is going to initialized to all 0's.
-When we find a host, we'll traverse it, looking for one of the following
-1) A record matching the host we just heard from
-2) An out of date record i.e. a record that is more than DEAD_THRESHOLD seconds old.
-If we find a matching record, we'll update the lastSeenAt time.
-Otherwise, we'll add the new record at the location of the out of date record.
-It is NOT guaranteed that the out of date records will be at the beginning of
-the array - in fact, this is likely to not be the case.
-Therefore, we must traverse the entire array to make sure we find a matching record.
-*/
-hostRecord* records;
 
 void addOrUpdateHost(char* newHostAddr);
 void checkHostsAliveSignalHandler(int sig);
